6109200409c53ac33c64bda9bc37b1d6a7162f8b
SatoshiTakiguchi <satoshi.takiguchi@tokyo.supersoftware.co.jp>
2022-04-11 14:07:40 +0900

    体裁調整

--- python.py
+++ python.py
@@ -6,18 +6,17 @@ import math
 for i in range(0,360,15): # 0度から345度まで15度ずつ繰り返す
     print(f"角度が{i}°の時")
     rad = math.radians(i) # 角度をラジアンに変換
-    print(f"con({i}°)は{round(math.cos(rad),4)}") #少数第4位まで表示する
-    print(f"sin({i}°)は{round(math.sin(rad),4)}")
+    print(f"con{i}°={round(math.cos(rad),4)}") #少数第4位まで表示する
+    print(f"sin{i}°={round(math.sin(rad),4)}")
     
     if(i in [90,270]): # tanの定義域処理
         print(f"tan{i}°は定義されてない")
     else:
-        print(f"tan({i}°)は{round(math.tan(rad),4)}")
+        print(f"tan{i}°={round(math.tan(rad),4)}")
     
 print('''
-以下、角度が360以上の時
-sin(角度÷360のあまり)
-cos(角度÷360のあまり)
-tan(角度÷360のあまり)
-と等しくなる
+以下
+sin(360°×n+t)=sint (n∈Z)
+cos(360°×n+t)=cost (n∈Z)
+tan(360°×n+t)=tant (n∈Z)
 ''')
